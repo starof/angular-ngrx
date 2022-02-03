c11b3a9f08c7f4a35046915fa3492dccbca3142f
starof <925309723@qq.com>
2022-02-03 20:51:27 +0800

    use entity

--- src/app/todo/store/reducers/todo.reducer.ts
+++ src/app/todo/store/reducers/todo.reducer.ts
@@ -1,5 +1,5 @@
-import { state } from '@angular/animations';
 import { Action, createReducer, on } from '@ngrx/store';
+import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
 import * as TodoActions from '../actions/todo.actions';
 
 export const todoFeatureKey = 'todo';
@@ -8,31 +8,33 @@ export interface Todo {
   id: string,
   title: string
 }
-export interface State {
-  todos: Todo[];
-
+export interface State extends EntityState<Todo> {
+  // additional entities state properties
 }
 
-export const initialState: State = {
-  todos: []
-};
+export const adapter: EntityAdapter<Todo> = createEntityAdapter<Todo>();
+
+export const initialState: State = adapter.getInitialState({
+  // additional entity state properties
+});
 
 export const reducer = createReducer(
   initialState,
-  on(TodoActions.addTodo, (state, action) => {
-    return {
-      ...state,
-      todos: [...state.todos,
-      {
-        id: Math.random().toString(36).substring(2, 9),
-        title: action.title
-      }]
-    };
-  }),
+  on(TodoActions.addTodo,
+    (state, action) => adapter.addOne({
+      id: Math.random().toString(36).substring(2, 9),
+      title: action.title
+    }, state)
+  ),
 
-  on(TodoActions.deleteTodo, (state, action) => ({
-    ...state,
-    todos: (<Todo[]>state.todos).filter(todo => todo.id !== action.id)
-  }))
-
+  on(TodoActions.deleteTodo,
+    (state, action) => adapter.removeOne(action.id, state)
+  )
 );
+
+export const {
+  selectIds,
+  selectEntities,
+  selectAll,
+  selectTotal,
+} = adapter.getSelectors();
--- src/app/todo/store/selectors/todo.selectors.ts
+++ src/app/todo/store/selectors/todo.selectors.ts
@@ -1,3 +1,4 @@
+import { adapter } from './../reducers/todo.reducer';
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromTodo from '../reducers/todo.reducer';
 
@@ -5,4 +6,9 @@ export const selectTodoState = createFeatureSelector<fromTodo.State>(
   fromTodo.todoFeatureKey
 );
 
-export const selectTodos = createSelector(selectTodoState,state=>state.todos);
+const { selectIds, //获取id集合，以数组形式呈现
+  selectEntities,//获取实体集合，以字典形式呈现
+  selectAll, //获取所有数据，以数组形式呈现
+  selectTotal //获取数据条数
+} = adapter.getSelectors();
+export const selectTodos = createSelector(selectTodoState, selectAll);
