d88c655052e748fd05a7bd047566a7df6cb04029
starof <925309723@qq.com>
2022-02-07 09:43:18 +0800

    rxjs quick start

--- src/app/learn-rxjs/quick-start/quick-start.component.ts
+++ src/app/learn-rxjs/quick-start/quick-start.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-quick-start',
@@ -10,6 +11,22 @@ export class QuickStartComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    //new Observable的时候传递一个函数，函数的参数是observer对象，在函数内部执行异步操作
+    const observable = new Observable((observer)=>{
+      setTimeout(()=>{
+        observer.next({name:"张三"})
+      },2000)
+    });
+
+    //observer是一个对象，观察者是一个对象，对象中有一个next属性，它是一个方法
+    const observer= {
+      next:(value:any)=>{
+        console.log(value);
+        
+      }
+    };
+
+    observable.subscribe(observer); 
   }
 
 }
