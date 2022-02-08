dbce2d5f27573e58920381989738603889166c8a
starof <925309723@qq.com>
2022-02-08 10:35:30 +0800

    订阅Subject不会立即执行

--- src/app/learn-rxjs/subject/subject.component.ts
+++ src/app/learn-rxjs/subject/subject.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-subject',
@@ -10,6 +11,13 @@ export class SubjectComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let demoSubject = new Subject();
+    //订阅的时候不会立即执行
+    demoSubject.subscribe((v)=>console.log("observer1" , v))
+    demoSubject.subscribe((v)=>console.log("observer2" , v))
+    setTimeout(()=>{
+      demoSubject.next(1);
+    },3000)
   }
 
 }
