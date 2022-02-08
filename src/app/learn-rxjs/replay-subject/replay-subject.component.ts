78308cfc6679e3ce6689baf362570f5e8433e606
starof <925309723@qq.com>
2022-02-08 10:53:11 +0800

    Replay subject新的订阅者可以拿到所有的历史结果

--- src/app/learn-rxjs/replay-subject/replay-subject.component.ts
+++ src/app/learn-rxjs/replay-subject/replay-subject.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-replay-subject',
@@ -10,6 +11,16 @@ export class ReplaySubjectComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let replaySubjectDemo = new ReplaySubject();
+    replaySubjectDemo.subscribe(v=>console.log("第一个观察者",v));
+    
+    replaySubjectDemo.next(1)
+    replaySubjectDemo.subscribe(v=>console.log("第二个观察者",v));
+    replaySubjectDemo.next(2)
+    replaySubjectDemo.next(3)
+    replaySubjectDemo.next(4)
+
+    replaySubjectDemo.subscribe(v=>console.log("第三个观察者",v)); //第三个观察者可以拿到1，2，3，4
   }
 
 }
