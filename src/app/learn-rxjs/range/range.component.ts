d1ec1c06d5d6d2fbab372651f405b61673b2e81c
starof <925309723@qq.com>
2022-02-09 08:54:20 +0800

    range and map operator

--- src/app/learn-rxjs/range/range.component.ts
+++ src/app/learn-rxjs/range/range.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { map, range } from 'rxjs';
 
 @Component({
   selector: 'app-range',
@@ -10,6 +11,12 @@ export class RangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    //每次发送一个值，调用一次next()
+    range(1,10).subscribe(console.log)
+    
+    console.log("调用map操作符");
+    
+    range(1,10).pipe(map(v=>v*10)).subscribe(console.log)
   }
 
 }
