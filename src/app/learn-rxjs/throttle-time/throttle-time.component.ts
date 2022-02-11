9368e25f5266c20eaf55c27ccc34fad178e66f83
starof <925309723@qq.com>
2022-02-11 21:02:41 +0800

    节流throttleTime 和 防抖debounceTime

--- src/app/learn-rxjs/throttle-time/throttle-time.component.ts
+++ src/app/learn-rxjs/throttle-time/throttle-time.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { debounceTime, fromEvent, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-throttle-time',
   templateUrl: './throttle-time.component.html',
   styleUrls: ['./throttle-time.component.scss']
 })
-export class ThrottleTimeComponent implements OnInit {
+export class ThrottleTimeComponent implements OnInit, AfterViewInit{
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  ngAfterViewInit(): void {
+    //example 1
+    // fromEvent(document,'mousemove')
+    // .pipe(
+    //   throttleTime(2000)
+    // ).subscribe(console.log)
+   
+    //example 2
+    //throttleTime 1s内只响应一次
+    // const clicks = fromEvent(document, 'click');
+    // const result = clicks.pipe(throttleTime(1000));
+    // result.subscribe(x => console.log(x));
+
+    //debounceTime 停止点击事件后1s响应
+    const clicks = fromEvent(document, 'click');
+    const result = clicks.pipe(debounceTime(1000));
+    result.subscribe(x => console.log(x));
   }
 
 }
