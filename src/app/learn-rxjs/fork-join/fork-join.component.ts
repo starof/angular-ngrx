6f022a65c956571e0011ca736b380e3184d44c07
starof <925309723@qq.com>
2022-02-11 09:53:45 +0800

    forkJoin

--- src/app/learn-rxjs/fork-join/fork-join.component.ts
+++ src/app/learn-rxjs/fork-join/fork-join.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { forkJoin, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-fork-join',
@@ -10,6 +11,31 @@ export class ForkJoinComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    const goodsObservable = new Observable((observer)=>{
+      var num=100;
+      setInterval(()=>{
+        observer.next(num++);
+        if(num===105){
+          observer.complete();
+        }
+      },1000)
+    })
+    const categoryObservable = new Observable((observer)=>{
+      var num=1;
+      setInterval(()=>{
+        observer.next(num++);
+        if(num===5){
+          observer.complete();
+        }
+      },1000)
+    })
+
+    forkJoin({
+      goods:goodsObservable,
+      category:categoryObservable
+    }).subscribe(console.log); //{goods: 104, category: 4}
+
   }
 
 }
