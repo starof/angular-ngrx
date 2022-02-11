a00f3c94dba4b0a5f36bfaaeb430d134e21d3f23
starof <925309723@qq.com>
2022-02-11 12:01:30 +0800

    fromEvent 给元素添加事件，将事件对象作为数据流输出  pluck获取数据流对象中的属性值

--- src/app/learn-rxjs/from-event/from-event.component.ts
+++ src/app/learn-rxjs/from-event/from-event.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import {filter, fromEvent, map, pluck, Subject, tap } from 'rxjs';
 
 @Component({
   selector: 'app-from-event',
   templateUrl: './from-event.component.html',
   styleUrls: ['./from-event.component.scss']
 })
-export class FromEventComponent implements OnInit {
-
+export class FromEventComponent implements AfterViewInit {
+  @ViewChild("nameInput") nameInput!:ElementRef;
   constructor() { }
 
   ngOnInit(): void {
+
+    
+  }
+  ngAfterViewInit(){
+    fromEvent<KeyboardEvent>(this.nameInput.nativeElement,'keyup').pipe(
+      filter(e=>e.key==="Enter"),
+      // tap(console.log),
+      // map(e=>(<HTMLInputElement>e.target).value)
+      pluck('target','value')
+    ).subscribe(console.log)
+
+    const btn = document.getElementById("btn")!;
+    fromEvent(btn,'click')
+    .pipe(
+      pluck('target')
+    ).subscribe(console.log)
   }
 
 }
