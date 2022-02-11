23054c35730014c4e040df21d5697cc06825ab7e
starof <925309723@qq.com>
2022-02-11 14:39:23 +0800

    switchMap 把按钮点击的Observable 转换为 发送请求的Observable

--- src/app/learn-rxjs/switch-map/switch-map.component.ts
+++ src/app/learn-rxjs/switch-map/switch-map.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ReducerManager } from '@ngrx/store';
+import { interval, take, Observable, range, switchMap, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-switch-map',
   templateUrl: './switch-map.component.html',
   styleUrls: ['./switch-map.component.scss']
 })
-export class SwitchMapComponent implements OnInit {
+export class SwitchMapComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    // const observable2 = range(100,10);
+
+    // interval(1000)
+    //   .pipe(take(5),
+    //   switchMap(e=>observable2)).subscribe(console.log)
+
+  }
+
+  ngAfterViewInit(): void {
+    const btn = document.getElementById('btn')!;
+    fromEvent(btn, 'click').pipe(
+      switchMap(e => interval(1000))
+    ).subscribe(console.log)
+
   }
 
 }
