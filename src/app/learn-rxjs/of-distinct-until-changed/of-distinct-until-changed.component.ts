6c76dcedb7f43e981b8ef2bc01b8f0ae161447de
starof <925309723@qq.com>
2022-02-12 11:24:00 +0800

    of() and distinctUntilChanged

--- src/app/learn-rxjs/of-distinct-until-changed/of-distinct-until-changed.component.ts
+++ src/app/learn-rxjs/of-distinct-until-changed/of-distinct-until-changed.component.ts
@@ -1,3 +1,4 @@
+import { Subject, of, distinctUntilChanged } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfDistinctUntilChangedComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    //example 1
+    of("a","b",[],{},true,20,null,undefined).subscribe(console.log)
+
+    console.log("------");
+    
+    //example 1
+    of(1, 1, 2, 2, 2, 1, 3).pipe(
+      distinctUntilChanged()
+    ).subscribe(console.log)
+
+    //example 2
+    const totallyDifferentBuilds$ = of(
+      { engineVersion: '1.1.0', transmissionVersion: '1.2.0' },
+      { engineVersion: '1.1.0', transmissionVersion: '1.4.0' },
+      { engineVersion: '1.3.0', transmissionVersion: '1.4.0' },
+      { engineVersion: '1.3.0', transmissionVersion: '1.5.0' },
+      { engineVersion: '2.0.0', transmissionVersion: '1.5.0' }
+    ).pipe(
+      distinctUntilChanged((prev, curr) => {
+        return (
+          prev.engineVersion === curr.engineVersion ||
+          prev.transmissionVersion === curr.transmissionVersion
+        );
+      })
+    );
+    totallyDifferentBuilds$.subscribe(console.log);
+
+    //example 3
+    const temps$ = of(30, 31, 20, 34, 33, 29, 35, 20);
+    const recordHighs$ = temps$.pipe(
+      distinctUntilChanged((prevHigh, temp) => {
+        // If the current temp is less than
+        // or the same as the previous record,
+        // the record hasn't changed.
+        return temp <= prevHigh;
+      })
+    );
+    recordHighs$.subscribe(console.log);
+    // Logs: 30, 31, 34, 35
+
+  }
 
   ngOnInit(): void {
   }
